f14098c557205396546c0f0a5b85f7c03144617e
Jonathan Hall <Jonathan.Hall@webappuk.com>
2013-10-06 23:30:47 +0100

    Add: Splitter handles which will only show between content regions within a row (handles between rows are not yet included).

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -40,22 +40,83 @@ describe('directives', function () {
                     expect(element.html()).not.toEqual('');
                 });
 
-                it('then it should provide an addRegionMethod on the scope', function () {
+                it('and it should provide an addRegionMethod on the scope', function () {
                     expect($scope.addRegion).toBeDefined();
                     expect($scope.addRegion).toEqual(jasmine.any(Function));
                 });
 
                 describe('and a region is added', function () {
-                    it('then it should render the result of compiling the supplied html', function () {
-                        var html = '<div>some html</div>',
-                            compiledHtml;
+                    var html = '<div>some html</div>',
+                        compiledHtml;
 
+                    beforeEach(function() {
                         $scope.addRegion(html);
                         compiledHtml = $compile(html)($rootScope);
                         $scope.$digest();
+                    });
 
+                    it('then it should render the result of compiling the supplied html',
+                        function () {
                         expect(compiledHtml.html()).not.toEqual('');
-                        expect(element.html()).toContain(compiledHtml.html());
+                        expect(element.html()).toContain(compiledHtml[0].outerHTML);
+                    });
+
+                    // TODO: These checks might be better suited to the E2E API?
+                    it('and it should not display a splitter handle', function() {
+                        var splitterHandles,
+                            i,
+                            len;
+
+                        splitterHandles = element[0].getElementsByClassName('wa-splitter-handle');
+
+                        for(i = 0, len = splitterHandles.length; i < len; i++) {
+                            expect(splitterHandles[i].outerHTML).toContain(
+                                'display: none'/* check all splitters are being hidden */
+                            );
+                        }
+                    });
+                });
+
+                describe('and two regions are added', function () {
+                    var html = '<div>some html</div>',
+                        html2 = '<div><span>some other html</span></div>',
+                        compiledHtml,
+                        compiledHtml2;
+
+                    beforeEach(function() {
+                        $scope.addRegion(html);
+                        compiledHtml = $compile(html)($rootScope);
+                        $scope.addRegion(html2);
+                        compiledHtml2 = $compile(html2)($rootScope);
+                        $scope.$digest();
+                    });
+
+                    it('then it should render the result of compiling both pieces of html',
+                        function () {
+                        expect(compiledHtml.html()).not.toEqual('');
+                        expect(compiledHtml2.html()).not.toEqual('');
+                        expect(element.html()).toContain(compiledHtml[0].outerHTML);
+                        expect(element.html()).toContain(compiledHtml2[0].outerHTML);
+                    });
+
+                    it('and it should render a splitter handle between the regions', function() {
+                        var endOfRegion1,
+                            startOfRegion2,
+                            htmlSubstring;
+
+                        endOfRegion1 = element.html().indexOf(compiledHtml[0].outerHTML) +
+                            compiledHtml[0].outerHTML.length;
+                        startOfRegion2 = element.html().indexOf(compiledHtml2[0].outerHTML);
+
+                        htmlSubstring = element.html().substr(endOfRegion1, (startOfRegion2 -
+                            endOfRegion1));
+
+                        expect(htmlSubstring).toContain(
+                            'wa-splitter-handle'/* class applied to splitter handle */
+                        );
+                        expect(htmlSubstring).not.toContain(
+                            'display: none'/* make sure the splitter isn't being hidden */
+                        );
                     });
                 });
             });
@@ -65,15 +126,19 @@ describe('directives', function () {
     describe('wa-content', function() {
         describe('given wa-content is applied as an attribute to a html element', function() {
             describe('when the value of wa-content is some valid html', function() {
-                it('then the element\'s content should be set to the result of compiling the value of wa-content', function() {
+                it('then the element\'s content should be set to the result of compiling the ' +
+                    'value of wa-content', function() {
                     var contentHtml = '<span>Some content!</span>',
                         element,
                         compiledHtml;
 
                     inject(function ($compile, $rootScope) {
-                        element = $compile('<div data-wa-content="\'' + contentHtml + '\'"></div>')($rootScope);
+                        element = $compile(
+                            '<div data-wa-content="\'' + contentHtml + '\'"></div>'
+                        )($rootScope);
                         compiledHtml = $compile(contentHtml)($rootScope);
-                        expect(element.html()).toContain(compiledHtml.html());
+
+                        expect(element.html()).toContain(compiledHtml[0].outerHTML);
                     });
                 })
             });
