2338084aa3244744fd242b3042978553808235f1
Jonathan Hall <Jonathan.Hall@webappuk.com>
2013-10-06 04:01:14 +0100

    Add: Initial development of waSplitter directive. Add: Skeleton of waContent directive to provide content addition/setting for waSplitter.

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,65 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive('appVersion', ['version', function (version) {
+        return function (scope, elm) {
+            elm.text(version);
+        };
+    }])
+
+    .directive('waSplitter', [function () {
+        return {
+            restrict: 'E',
+            template: '<div>' +
+                '<div data-ng-repeat="regionlist in regions">' +
+                '<div data-ng-repeat="region in regionlist" data-wa-content="region"></div>' +
+                '</div>' +
+                '</div>',
+            controller: function ($scope) {
+                $scope.regionProps = {
+                    regions: []
+                };
+
+                $scope.addRegion = function (html, rowIndex) {
+                    rowIndex = rowIndex || 0;
+                    $scope.regionProps.regions[rowIndex] = $scope.regionProps.regions[rowIndex] || [];
+                    $scope.regionProps.regions[rowIndex].push(html);
+                };
+            }
+        }
+            ;
+    }])
+
+    .
+    directive('waContent', ['$compile', function ($compile) {
+        return {
+            link: function ($scope, $element, $attrs) {
+                var onContentChanged = function (content) {
+                    var strippedContent,
+                        linkFn,
+                        el;
+
+                    strippedContent = (content || '').replace(/^'/, '');
+                    strippedContent = strippedContent.replace(/'$/, '');
+
+                    try {
+                        linkFn = $compile(strippedContent);
+                    } catch (ex) {
+                    }
+
+                    if (linkFn) {
+                        el = linkFn($scope);
+                        $element.innerHTML = '';
+                        $element.append(el);
+                    }
+                };
+
+                onContentChanged($attrs.waContent);
+                $scope.$watch(function () {
+                    return $attrs.waContent;
+                }, function (value) {
+                    onContentChanged(value);
+                });
+            }
+        };
+    }]);
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -2,18 +2,82 @@
 
 /* jasmine specs for directives go here */
 
-describe('directives', function() {
-  beforeEach(module('myApp.directives'));
+describe('directives', function () {
+    beforeEach(module('myApp.directives'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
+    describe('app-version', function () {
+        it('should print current version', function () {
+            module(function ($provide) {
+                $provide.value('version', 'TEST_VER');
+            });
+            inject(function ($compile, $rootScope) {
+                var element = $compile('<span data-app-version></span>')($rootScope);
+                expect(element.text()).toEqual('TEST_VER');
+            });
+        });
     });
-  });
+
+    describe('wa-splitter', function () {
+        describe('given cogito ergo sum', function () {
+            describe('when the directive\'s tag is compiled', function () {
+                var directiveTag = '<wa-splitter></wa-splitter>',
+                    $scope,
+                    element,
+                    $compile,
+                    $rootScope;
+
+                beforeEach(function () {
+                    inject(function (_$compile_, _$rootScope_) {
+                        $compile = _$compile_;
+                        $rootScope = _$rootScope_;
+                    });
+                    $scope = $rootScope.$new();
+                    element = $compile(directiveTag)($scope);
+                });
+
+                it('then it should transform the tag', function () {
+                    expect(element.html()).not.toEqual(directiveTag);
+                    expect(element.html()).not.toEqual('');
+                });
+
+                it('then it should provide an addRegionMethod on the scope', function () {
+                    expect($scope.addRegion).toBeDefined();
+                    expect($scope.addRegion).toEqual(jasmine.any(Function));
+                });
+
+                describe('and a region is added', function () {
+                    // TODO: Get this 'addRegion' method working correctly
+                    xit('then it should render the result of compiling the supplied html', function () {
+                        var html = '<div>some html</div>',
+                            compiledHtml;
+
+                        $scope.addRegion(html);
+                        compiledHtml = $compile(html)($rootScope);
+                        $scope.$digest();
+
+                        expect(compiledHtml.html()).not.toEqual('');
+                        expect(element.html()).toContain(compiledHtml.html());
+                    });
+                });
+            });
+        });
+    });
+
+    describe('wa-content', function() {
+        describe('given wa-content is applied as an attribute to a html element', function() {
+            describe('when the value of wa-content is some valid html', function() {
+                it('then the element\'s content should be set to the result of compiling the value of wa-content', function() {
+                    var contentHtml = '<span>Some content!</span>',
+                        element,
+                        compiledHtml;
+
+                    inject(function ($compile, $rootScope) {
+                        element = $compile('<div data-wa-content="\'' + contentHtml + '\'"></div>')($rootScope);
+                        compiledHtml = $compile(contentHtml)($rootScope);
+                        expect(element.html()).toContain(compiledHtml.html());
+                    });
+                })
+            });
+        });
+    });
 });
