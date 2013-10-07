c4cc2a2c7bb5d38501df63614aeed230f6add1f7
Jon Hall <jonathan.hall09@hotmail.com>
2013-10-07 23:58:12 +0100

    Remove: Failing E2E tests - leaving the code though as it demonstrates protractor usage for automated integration testing even though the tests themselves are no longer valid against the AngularJS homepage.

--- test/e2e/test-scenario.js
+++ test/e2e/test-scenario.js
@@ -5,20 +5,20 @@ describe('angularjs homepage', function() {
         ptor = protractor.getInstance();
         ptor.get('http://www.angularjs.org');
 
-        ptor.findElement(protractor.By.input("yourName")).sendKeys("Julie");
+        //ptor.findElement(protractor.By.input("yourName")).sendKeys("Julie");
 
-        var greeting = ptor.findElement(protractor.By.binding("{{yourName}}!"));
+        //var greeting = ptor.findElement(protractor.By.binding("{{yourName}}!"));
 
-        expect(greeting.getText()).toEqual('Hello Julie!');
+        //expect(greeting.getText()).toEqual('Hello Julie!');
     });
 
     it('should list todos', function() {
         ptor.get('http://www.angularjs.org');
 
-        var todo = ptor.findElement(
-            protractor.By.repeater('todo in todos').row(2));
+        //var todo = ptor.findElement(
+        //    protractor.By.repeater('todo in todos').row(2));
 
-        expect(todo.getText()).toEqual('build an angular app');
+        //expect(todo.getText()).toEqual('build an angular app');
     });
 
     // Uncomment to see failures.
